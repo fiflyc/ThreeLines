53cb58d1e3492dff1500e7e7ebdbcdd907833185
FiFlyC <combolev@gmail.com>
2020-10-07 19:08:14 +0300

    Added russian keyboard support

--- src/main/java/com/example/threelines/PCController.java
+++ src/main/java/com/example/threelines/PCController.java
@@ -10,21 +10,27 @@ public class PCController implements Controller {
 
     public void pressedKey(String key) {
         switch (key.toLowerCase()) {
+            case "ф":
             case "a":
                 model.sendCommand(Control.LEFT);
                 break;
+            case "в":
             case "d":
                 model.sendCommand(Control.RIGHT);
                 break;
+            case "ц":
             case "w":
                 model.sendCommand(Control.UP);
                 break;
+            case "ы":
             case "s":
                 model.sendCommand(Control.DOWN);
                 break;
+            case "с":
             case "c":
                 model.sendCommand(Control.SELECT);
                 break;
+            case "у":
             case "e":
                 model.sendCommand(Control.UNSELECT);
                 break;
